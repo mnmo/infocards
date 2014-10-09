d3c1995342413d09958ffee04d269fd657e4bcb2
Fabricio C Zuardi <fabricio@fabricio.org>
2014-10-09 16:33:43 -0300

    styling for the contact form to be placed in the footer, not collapsible yet

--- wordpress/wp-content/themes/infocards/footer.php
+++ wordpress/wp-content/themes/infocards/footer.php
@@ -38,13 +38,18 @@
 	    </ul>
 	  </div>
 	  <div class="sitemap">
-			<?php wp_nav_menu( array( 
-                          'menu' => 'sitemap', 
-                          'container' => 'div', 
-                          'container_class' => 'menu', 
+			<?php wp_nav_menu( array(
+                          'menu' => 'sitemap',
+                          'container' => 'div',
+                          'container_class' => 'menu',
                           'menu_class' => ''
                           ) ); ?>
 	  </div>
+<?php
+	if( function_exists( 'ninja_forms_display_form' ) ){
+		 ninja_forms_display_form( 1 );
+	}
+?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/prefixfree.min.js" type="text/javascript"></script>
